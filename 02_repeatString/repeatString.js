d043c6414bc3e98d44e5777cc0f98e58677fb70f
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-26 23:31:02 +0300

    Add function to multiply strin n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let output = string;
 
+    if (num ==0){
+        return ""
+    }
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        while(num !=0, --num){
+            output +=string
+        }
+        return output
+    }
+    
 };
 
 // Do not edit below this line
