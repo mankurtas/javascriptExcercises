490f7a9e754adb7934a87f5e569bc347212a804f
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-29 13:00:21 +0300

    Create function to get first argument as array and n argumetns. Function remoes form array dublicates and if found same item as any argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
-};
+    let modifiedArray = arguments[0]
+    let argumentsLength = arguments.length
+    
+    let withoutDublicates = modifiedArray.filter((item, index) => modifiedArray.indexOf(item) === index)
+    
+    for (let i = 1; i < argumentsLength; i++){
+        let founded =  withoutDublicates.find((x) => x === arguments[i])
+
+        if(founded){
+          withoutDublicates.splice(withoutDublicates.indexOf(founded),1)
+        }
+    }
+
+   
+    return withoutDublicates
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
