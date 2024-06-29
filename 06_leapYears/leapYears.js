23e7fee335df01e7151b89f3016f153d62436972
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-29 14:54:35 +0300

    Function to check leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(years) {
+    
+let isLeaYear = true;
+
+      if ((years%100 !=0 && years %4 ==0) || (years%400 ==0) ){
+        isLeaYear = true
+      }  
+
+      else {
+        isLeaYear =false
+      }
+
+return isLeaYear
 
 };
 
