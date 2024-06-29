7905c5dd3d3d1822cb9d75673f50a26cd72d0c56
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-29 13:21:04 +0300

    Function to add all intigares based on provided range by to arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+
+    let sumTotal = 0;
+
+    if(Number.isInteger(num1) && Number.isInteger(num2) && num1 > -1 && num2 > -1) {
+        if(num1>num2){
+
+            for (let i = num2; i < num1+1; i++){
+                
+                sumTotal +=i;
+            }
+
+        }else{
+            for (let i = num1; i < num2+1; i++){
+                
+                sumTotal +=i;
+            }
+        }
+
+
+    return sumTotal
+
+    }
+
+    else {
+        return "ERROR"
+    }
 
 };
 
