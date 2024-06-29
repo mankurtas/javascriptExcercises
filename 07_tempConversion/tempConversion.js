c497ace95a2ccca4bc9b966ca6d7baa74b64b505
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-29 19:40:50 +0300

    Function to convert temt to Fahrenheit and celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheits) {
+
+  return parseFloat(((tempFahrenheits - 32) *5 / 9).toFixed(1))
+  
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+
+return parseFloat(((tempCelsius * 9 / 5) + 32).toFixed(1))
+
 };
 
 // Do not edit below this line
