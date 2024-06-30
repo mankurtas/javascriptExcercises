9268c32a1ef53bf25e9fcda0390e832c76da4624
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-30 23:32:52 +0300

    Check does string is palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,32 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+    let lowerCaseText = text.toLowerCase()
+
+    let textArray = lowerCaseText.split('')
+
+    // let lettersArray = []
+
+    // tesxArray.forEach((item) => {
+    //     if(item !==" " || item !=="," || item !==".") {
+    //         lettersArray.push(item)
+    //     }
+    // });
+
+    textArray.forEach((item, index) => {
+        if(item === "," || item === "." || item ==="!"){
+            textArray.splice(index,1)
+        }
+    });
+
+    textArray = textArray.filter(item => item !==" ")
+
+    let reversedArray = []
+
+    textArray.forEach(item => reversedArray.unshift(item))
+
+//    let reversedArray = tesxArray.reverse()
+
+    return textArray.join("") === reversedArray.join("")
 
 };
 
