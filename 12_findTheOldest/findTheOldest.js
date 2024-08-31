a8222be97e36a4d80df34b4a0ca13cc2f2f09584
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-08-31 22:31:45 +0300

    Add function to finde oldest persons

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
-};
+    let oldessPersonName = arr.reduce((oldest, current) =>{
+
+        let yearOfDeath = new Date().getFullYear();
+        let yearOfBirth = current.yearOfBirth
+
+        if(current.hasOwnProperty('yearOfDeath')){
+             yearOfDeath = current.yearOfDeath
+        }
+
+        let currentAge = yearOfDeath - yearOfBirth
+
+        let yearOfDeathOldest = new Date().getFullYear();
+        let yearOfBirthOldest = oldest.yearOfBirth
+        if(oldest.hasOwnProperty('yearOfDeath')){
+             yearOfDeathOldest = oldest.yearOfDeath
+        }
+        let oldestAge = yearOfDeathOldest - yearOfBirthOldest
+
+        return currentAge > oldestAge ? current : oldest
+
+    }, arr[0])
+
+    return oldessPersonName
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
