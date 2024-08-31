3f22345894c86acb36d76050d669d09ed1dc073a
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-08-31 13:20:02 +0300

    Add functionality to Finonacci sequience

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,32 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibonacciArray = []
+    if(num == 0) {
+        fibonacciArray = [0]
+        return fibonacciArray[0]
+    } 
+    
+    
+    else if (num < 0) {
+        return "OOPS"
+    }
+   else{
+    let firstIteme = 1
+    // const fibonacciArray = []
+    fibonacciArray.push(firstIteme)
+    fibonacciArray.push(firstIteme)
+    
+    for (let index = 2; index < num; index++) {
+
+      
+
+        fibonacciArray.push(fibonacciArray[index-2] + fibonacciArray[index-1])
+
+
+    }
+   
+    return fibonacciArray[fibonacciArray.length - 1]
+   }
+
 
 };
 
